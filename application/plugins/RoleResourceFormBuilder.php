0df917ad40e7a5647f01927185b3b404cbfc1171
Szurovecz János <szjani@szjani.hu>
2011-01-03 08:39:09 +0100

    persmission form elements order

--- application/plugins/RoleResourceFormBuilder.php
+++ application/plugins/RoleResourceFormBuilder.php
@@ -7,21 +7,24 @@ class RoleResourceFormBuilder extends FormBuilder {
   protected $ignoredFields = array('role');
 
   public function postVisit() {
+    $this->getForm()->getElement('resource')->setOrder(1);
+    $this->getForm()->getElement('privilege')->setOrder(2);
+    $this->getForm()->getElement('allowed')->setOrder(3);
+    $this->getForm()->getElement('save')->setOrder(10);
+
     $role = new \Zend_Dojo_Form_Element_FilteringSelect('role');
     $role
-      ->setOrder(2)
+      ->setOrder(0)
       ->setLabel('entities/RoleResource/role');
     $this->getForm()->addElement($role);
-//    $role = $this->getForm()->getElement('role');
+
     /* @var $role \Zend_Dojo_Form_Element_FilteringSelect */
-    if ($role) {
-      $role
-        ->setAutocomplete(true)
-        ->setStoreId('roleResourceStore')
-        ->setStoreType('dojox.data.QueryReadStore')
-        ->setStoreParams(array('url' => '/admin/role/autocomplete'))
-        ->setAttrib("searchAttr", "role");
-    }
+    $role
+      ->setAutocomplete(true)
+      ->setStoreId('roleResourceStore')
+      ->setStoreType('dojox.data.QueryReadStore')
+      ->setStoreParams(array('url' => '/admin/role/autocomplete'))
+      ->setAttrib("searchAttr", "role");
     
     if ($this->entity->getRole() instanceof \entities\Role) {
       $entityRole = $this->entity->getRole();
