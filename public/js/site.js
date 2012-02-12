1fc5ec1d6efaf5a78b4e7397719ee99669a925fa
Szurovecz János <szjani@szjani.hu>
2012-02-12 20:32:20 +0100

    update icon added, new translating

--- public/js/site.js
+++ public/js/site.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
   $('.nav a.list').prepend('<i class="icon-list"></i> ');
   $('.nav a.create').prepend('<i class="icon-plus-sign"></i> ');
+  $('.nav a.update').prepend('<i class="icon-edit"></i> ');
 });
