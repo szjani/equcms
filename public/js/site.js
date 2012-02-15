82f5007a013f73347c71749078a3102e130a3d90
János Szurovecz <janos.szurovecz@factory.co.hu>
2012-02-15 16:13:39 +0100

    remove submit button value from URL when submit form with get method

--- public/js/site.js
+++ public/js/site.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
   $('.nav a.list').prepend('<i class="icon-list"></i> ');
   $('.nav a.create').prepend('<i class="icon-plus-sign"></i> ');
   $('.nav a.update').prepend('<i class="icon-edit"></i> ');
+  
+  // disable URL parameter
+  $('input[type="submit"]', $('form[method="get"]')).each(function() {
+    $(this).removeAttr('name');
+  });
 });
